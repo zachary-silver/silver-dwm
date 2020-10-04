7cb6f7aa68af93e2b9ea1b68d96579685fe28487
Zachary Silver <zmansilver@gmail.com>
2020-10-03 23:14:17 -0700

    updated config for dwmbar gaps

--- config.def.h
+++ config.def.h
@@ -7,12 +7,12 @@ static const unsigned int gappih    = 30;       /* horiz inner gap between windo
 static const unsigned int gappiv    = 30;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 23;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 28;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 0;       	/* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const int vertpad            = 0;       	/* vertical padding of bar */
-static const int sidepad            = 0;       	/* horizontal padding of bar */
+static const int user_bh            = 28;       	/* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int vertpad            = 12;       	/* vertical padding of bar */
+static const int sidepad            = 27;       	/* horizontal padding of bar */
 static const char *fonts[]          = { "Hermit:size=11:style=bold" };
 static const char dmenufont[]       = "Hermit:size=11:style=bold";
 static const char col_navy[]        = "#232731";
@@ -34,7 +34,7 @@ static const char *colors[][3]      = {
 static const int statmonval = 0;
 
 /* tagging */
-static const char *tags[] = { "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  " };
+static const char *tags[] = { "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  " };
 static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
@@ -49,11 +49,11 @@ static const Rule rules[] = {
 	{ "feh",				NULL,	    	NULL,			0,		1,	    -1 },
 	{ "Surf",				NULL,	    	NULL,			1 << 2,		0,	    -1 },
 	{ "firefox",  	 			NULL,       	NULL,      		1 << 2,       	0,          -1 },
-	{ "Evince", 	 			NULL,	    	NULL,			1 << 3,		0,	    -1 },
-	{ NULL,					NULL,    	"LibreOffice",		1 << 4,		0,	    -1 },
-	{ "Gimp",     	 			NULL,       	NULL,       		1 << 4,      	0,          -1 },
-	{ "spotify",				NULL,	    	NULL,			1 << 5,		0,	    -1 },
-	{ "discord",	 			NULL,	    	NULL,			1 << 6,		0,	    -1 },
+	{ "spotify",				NULL,	    	NULL,			1 << 3,		0,	    -1 },
+	{ "discord",	 			NULL,	    	NULL,			1 << 4,		0,	    -1 },
+	{ "Evince", 	 			NULL,	    	NULL,			1 << 5,		0,	    -1 },
+	{ NULL,					NULL,    	"LibreOffice",		1 << 6,		0,	    -1 },
+	{ "Gimp",     	 			NULL,       	NULL,       		1 << 6,      	0,          -1 },
 	{ "VirtualBox Manager", 		NULL,	    	NULL,			1 << 8,		0,	    -1 },
 };
 
